19cf1f9832a8cafc56af85d45bd7ba1d6fa260ac
nerina.szucs <sz.nerina@gmail.com>
2021-11-29 17:48:00 +0100

    Splitter scripts updated
    TXTsplitter.py calls multiple_splitter() function from TXTsplitter_multiple.py when needed & fixed more issues

--- scripts/TXTsplitter.py
+++ scripts/TXTsplitter.py
@@ -1,228 +1,154 @@
-from getXMLdata import get_name, get_filenames, get_creationsbyfile
+from getXMLdata import get_name, get_formattedname, get_filenames, get_creationsbyfile
+from TXTsplitter_multiple import multiple_splitter
 from TXTpreprocessing import preprocessing
 
-xmlfile = 'xmloutputs/Csikós__Zsuzsanna.xml'
-creator_name = get_name(xmlfile)
-print(creator_name)
+start = input("Induljon a szkript? (igen) ")
+restart = "igen"
+while start == "igen" and restart == "igen":
 
-filenames = get_filenames(xmlfile)
-filename = filenames[4]
-print(filename)
+    xmlinput = input("XML fájl neve (fajlnev.xml): ")
+    xmlfile = f'xmloutputs/{xmlinput}'
+    creator_name = get_name(xmlfile)
+    # a fájlnevek konvenciójának megfelelően
+    creator_name_f = get_formattedname(xmlfile).replace(' ', '_')
+    rev_creator_name = creator_name_f.split('__')[1] + creator_name_f.split('__')[0]
+    # pl. Csobánka__Zsuzsa Emese -> Csobánka__Zsuzsa_Emese
+    creationsbyfile = get_creationsbyfile(xmlfile)
 
-creationsbyfile = get_creationsbyfile(xmlfile)
-print(creationsbyfile)
+    k = -1
+    items = creationsbyfile.items()
+    for item in items:
+        k += 1
+        print(str(k) + ": " + str(item))
 
-addition = ""
-for key in creationsbyfile.keys():
-    if key == filename:
-        if ";" in creationsbyfile[key]:
-            addition = filename.split('_', 1)[1]
-        else:
-            if '[' in creationsbyfile[key] and ':' in creationsbyfile[key]:
-                addition = creationsbyfile[key].split('[')[0].replace(':', '').strip() + ".txt"
-            if '?' in creationsbyfile[key]:
-                addition = creationsbyfile[key].replace('?', '').strip() + ".txt"
-            elif '[' in creationsbyfile[key]:
-                addition = creationsbyfile[key].split('[')[0].strip() + ".txt"
-            elif '(' in creationsbyfile[key]:
-                addition = creationsbyfile[key].split('(')[0].strip() + ".txt"
-            elif ':' in creationsbyfile[key]:
-                addition = creationsbyfile[key].replace(':', '').strip() + ".txt"
+    print("Választott szerző neve: " + creator_name)
+    count = int(input("Hanyadik fájl legyen darabolva? "))
+    filenames = get_filenames(xmlfile)
+    filename = filenames[count]
+    print("Darabolás: " + filename)
+
+    addition = ""
+    multiple = False
+    for key in creationsbyfile.keys():
+        if key == filename:
+            if ";" in creationsbyfile[key]:
+                multiple = True
+                addition = filename.split('_', 1)[1]
             else:
-                addition = creationsbyfile[key] + ".txt"
+                addition = creationsbyfile[key]
+                if '?' in addition:
+                    addition = addition.replace('?', '').strip().replace('  ', ' ')
+                if '[' in addition:
+                    addition = addition.split('[')[0].strip().replace('  ', ' ')
+                if '(' in addition:
+                    addition = addition.split('(')[0].strip().replace('  ', ' ')
+                if ':' in addition:
+                    addition = addition.replace(':', '').strip().replace('  ', ' ')
+                if '"' in addition or "„" in addition:
+                    addition = addition.replace('"', '').replace('„', '').strip().replace('  ', ' ')
+                addition = addition + ".txt"
 
-# for currentTitle in creationsbyfile.values():
-#     if ";" in currentTitle:
-#         titles = currentTitle.split(';')
-#
-#         for i in range(len(titles)):
-#             titles[i] = titles[i].strip()
-#             if '[' in titles[i]:
-#                 splitted = titles[i].split('[')
-#                 titles[i] = splitted[0]
-#                 addition = filename
-#
-#     else:
-#         addition = currentTitle + '.txt'
+    creators_file = open("../data/creators.txt", "r", encoding="utf-8")
+    content = creators_file.read()
+    other_creators = content.split("\n")
+    other_creators.pop()
+    creators_file.close()
 
-creators_file = open("../data/creators.txt", "r", encoding="utf-8")
-content = creators_file.read()
-other_creators = content.split("\n")
-other_creators.pop()
-creators_file.close()
+    bigcreatorname = creator_name.upper().replace(' ', '')
+    bigcreatorname_rev = rev_creator_name.upper().replace(' ', '')
 
-bigcreatorname = creator_name.upper().replace(' ', '')
+    extended_list = []
 
-extended_list = []
+    for i in range(len(other_creators)):
+        # kezdő \t elhagyása - Abafáy-Deák\tCsillag
+        other_creators[i] = other_creators[i][1:]
+        # darabolás \t mentén - ['Abafáy-Deák', 'Csillag']
+        separate = other_creators[i].split('\t')
+        normal_name = separate[0] + separate[1]
+        # 'ABAFÁY-DEÁKCSILLAG'
+        normal_name = normal_name.strip().upper().replace(' ', '')
+        rev_name = separate[1] + separate[0]
+        # 'CSILLAGABAFÁY-DEÁK'
+        rev_name = rev_name.strip().upper().replace(' ', '')
+        extended_list.append(normal_name)
+        extended_list.append(rev_name)
 
-for i in range(len(other_creators)):
-    # kezdő \t elhagyása - Abafáy-Deák\tCsillag
-    other_creators[i] = other_creators[i][1:]
-    # darabolás \t mentén - ['Abafáy-Deák', 'Csillag']
-    separate = other_creators[i].split('\t')
-    normal_name = separate[0] + separate[1]
-    # 'ABAFÁY-DEÁKCSILLAG'
-    normal_name = normal_name.strip().upper().replace(' ', '')
-    rev_name = separate[1] + separate[0]
-    # 'CSILLAGABAFÁY-DEÁK'
-    rev_name = rev_name.strip().upper().replace(' ', '')
-    extended_list.append(normal_name)
-    extended_list.append(rev_name)
+    extended_list = list(dict.fromkeys(extended_list))
+    extended_list.append("HIVATKOZOTTÉSFÖLHASZNÁLTIRODALOM")
+    extended_list.append("JEGYZETEK")
+    extended_list.append("FORRÁSOK")
+    extended_list.append("FELHASZNÁLT IRODALOM")
 
-extended_list = list(dict.fromkeys(extended_list))
-extended_list.append("HIVATKOZOTTÉSFÖLHASZNÁLTIRODALOM")
-extended_list.append("JEGYZETEK")
+    with open(f"../data/txt/{filename}", encoding="utf-8", errors="ignore") as in_file:
+        in_file_lines = in_file.readlines()
 
-# __ beszúrása a vezeték- és keresztnév közé, a fájlnevek konvenciójának megfelelően
-creator_name_f = creator_name.replace(" ", "__")
-with open(f"../data/txt/{filename}", encoding="utf-8", errors="ignore") as in_file:
-    in_file_lines = in_file.readlines()
+        for i in range(len(in_file_lines)):
+            # előkészítő szkript + szóközök, tabulátorok eltávolítása + darabolás soronként
+            in_file_lines[i] = preprocessing(in_file_lines[i]).replace(' ', '').replace('	', '').strip('\n')
 
-    for i in range(len(in_file_lines)):
-        # előkészítő szkript + szóközök, tabulátorok eltávolítása + darabolás soronként
-        in_file_lines[i] = preprocessing(in_file_lines[i]).replace(' ', '').replace('	', '').strip('\n')
+        creator_line_indexes = []
+        creator_line_names = []
 
-    creator_line_indexes = []
-    creator_line_names = []
+    for i in range(0, len(in_file_lines)):
+        startcreators = next((k for k in extended_list if in_file_lines[i].startswith(k)), None)
+        if startcreators is not None:
+            creator_line_indexes.append(i + 1)
+            creator_line_names.append(startcreators)
 
-for i in range(0, len(in_file_lines)):
-    startcreators = next((k for k in extended_list if in_file_lines[i].startswith(k)), None)
-    if startcreators is not None:
-        creator_line_indexes.append(i + 1)
-        creator_line_names.append(startcreators)
-
-
-print(creator_line_names)
-print(creator_line_indexes)
-
-
-def find_index(namelist, indexlist, creatorname):
-    authorpos = []
-    otherauthorpos = []
-    indexes = []
-    for g in range(0, len(namelist)):
-        if namelist[g] == creatorname:
-            authorpos.append(indexlist[g])
-            otherauthorpos.append(indexlist[g + 1])
-    indexes.append(authorpos[1])
-    indexes.append(otherauthorpos[1])
-    return indexes
+    def find_index(namelist, indexlist, creatorname, rev):
+        authorpos = []
+        otherauthorpos = []
+        indexes = []
+        for g in range(0, len(namelist)):
+            if namelist[g] == creatorname or namelist[g] == rev:
+                authorpos.append(indexlist[g])
+                otherauthorpos.append(indexlist[g + 1])
+        indexes.append(authorpos[1])
+        indexes.append(otherauthorpos[1])
+        return indexes
 
 
-indexes = find_index(creator_line_names, creator_line_indexes, bigcreatorname)
+    indexes = find_index(creator_line_names, creator_line_indexes, bigcreatorname, bigcreatorname_rev)
 
-done = False
-lineindex = 0
-with open(f"../data/txt/{filename}", encoding="utf-8", errors="ignore") as in_file:
-    while not done:
-        with open(f"txtoutputs/{creator_name_f}_{addition}", "w", encoding="windows-1250", errors="ignore") as out_file:
-            while not done:
-                global line_name1
-                try:
-                    line = preprocessing(next(in_file))
-                    lineindex += 1
-                except StopIteration:
-                    done = True
-                    break
-                # ha a keresett szerzo neve
-                line_name1 = False
-                if lineindex == indexes[0]:
-                    line_name1 = line
-                    # hagyja ki es folytassa
-                    break
-                # ha egy masik szerzo neve
-                if lineindex == indexes[1]:
-                    # ha nem a keresett szerzo neve
-                    if line_name1 != line and line_name1 != "None":
+    done = False
+    lineindex = 0
+    with open(f"../data/txt/{filename}", encoding="utf-8", errors="ignore") as in_file:
+        while not done:
+            with open(f"txtoutputs/{creator_name_f}_{addition}", "w", encoding="windows-1250",
+                      errors="ignore") as out_file:
+                while not done:
+                    global line_name1
+                    try:
+                        line = preprocessing(next(in_file))
+                        lineindex += 1
+                    except StopIteration:
                         done = True
-                else:
-                    out_file.write(line + '\n')
+                        break
+                    # ha a keresett szerzo neve
+                    line_name1 = False
+                    if lineindex == indexes[0]:
+                        line_name1 = line
+                        # hagyja ki es folytassa
+                        break
+                    # ha egy masik szerzo neve
+                    if lineindex == indexes[1]:
+                        # ha nem a keresett szerzo neve
+                        if line_name1 != line and line_name1 != "None":
+                            done = True
+                    else:
+                        out_file.write(line + '\n')
 
-# print(' '.encode('utf-16'))
-# print('	'.encode('utf-16'))
+    if done is True:
+        print("Kész! ")
+        if multiple is True:
+            output = f"{creator_name_f}_{addition}"
+            print(f"A(z) {output} nevű fájl több művet tartalmaz. Továbbdarabolás... ")
+            # "Abafáy-Deák__Csillag_2020_002.txt", dict{'tiszataj_??.txt', 'Asd;Asd;Asd'}
+            multiple_splitter(output, creationsbyfile, creator_name_f)
+            print("Kész! ")
+            restart = input("Újra? (igen/nem) ")
+        else:
+            restart = input("Újra? (igen/nem) ")
 
-# done = False
-# with open(f"../data/txt/{filename}", encoding="utf-8", errors="ignore") as in_file:
-#     while not done:
-#         with open(f"txtoutputs/{creator_name_f}.txt", "w", encoding="windows-1250", errors="ignore") as out_file:
-#             while not done:
-#                 global line_name1
-#                 try:
-#                     line = preprocessing(next(in_file))
-#                     line_nospace = line.replace(" ", "")
-#                 except StopIteration:
-#                     done = True
-#                     break
-#                 # ha a keresett szerzo neve
-#                 line_name1 = False
-#                 if any(e in line for e in creator_variations):
-#                     line_name1 = line
-#                         # hagyja ki es folytassa
-#                     break
-#                     # ha szokozokkel, de benne volt a keresett szerzo
-#                 elif any(e in line_nospace for e in creator_variations):
-#                     line_name1 = line
-#                     break
-#                 # ha egy masik szerzo neve
-#                 if any(f in line for f in extended_list):
-#                         # ha nem a keresett szerzo neve
-#                     if line_name1 != line and line_name1 != "None":
-#                         done = True
-#                 else:
-#                     out_file.write(line + '\n')
-
-# for creators, filelines in zip_longest(extended_list, in_file_lines):
-#     if creators in filelines:
-#         testnames.append(creators)
-
-# for key in creator_line_indexes:
-#     for value in creator_line_names:
-#         merged[key] = value
-#         creator_line_names.remove(value)
-#         break
-
-# def find_indexes(dictionary, authorname):
-# authorpos = []
-# nextauthorpos = 0
-# indexes = []
-#
-# for key, value in dictionary.items():
-#     if value == authorname:
-#         authorpos.append(key)
-#         nextauthorpos = dictionary[key]
-#
-# indexes.append(authorpos[1])
-# indexes.append(nextauthorpos)
-# return indexes
-
-# for i in range(0, len(in_file_lines)):
-#     for elem in extended_list:
-#         # 46. sor nem jo
-#         substring = elem
-#         if substring in in_file_lines[i] or in_file_lines[i] in extended_list:
-#             creator_line_indexes.append(i + 1)
-#             creator_line_names.append(in_file_lines[i])
-#         break
-# for i in range(0, len(in_file_lines)):
-#     # 46. sor nem jo
-#     substring = extended_list[i]
-#     if substring in in_file_lines[i] or in_file_lines[i] in extended_list:
-#         creator_line_indexes.append(i + 1)
-#         creator_line_names.append(in_file_lines[i])
-# for i in range(0, len(in_file_lines)):
-#     for j in range(0, len(extended_list)):
-#         matching = [s for s in in_file_lines if extended_list[j] in s]
-#         if in_file_lines[i] in extended_list:
-#             creator_line_indexes.append(i + 1)
-#             creator_line_names.append(in_file_lines[i])
-#         print(matching)
-#     break
-
-# for i in range(0, len(in_file_lines)):
-#     startcreators = next((k for k in extended_list if in_file_lines[i].startswith(k)), None)
-#     if in_file_lines[i] in extended_list:
-#         creator_line_indexes.append(i + 1)
-#         creator_line_names.append(in_file_lines[i])
-#     elif startcreators is not None:
-#         creator_line_indexes.append(i + 1)
-#         creator_line_names.append(startcreators)
+    # print(' '.encode('utf-16'))
+    # print('	'.encode('utf-16'))
--- scripts/TXTsplitter_multiple.py
+++ scripts/TXTsplitter_multiple.py
@@ -1,43 +1,59 @@
-from getXMLdata import get_creationsbyfile
+from getXMLdata import get_creationsbyfile, get_formattedname
 
-creations = get_creationsbyfile('xmloutputs/Benedek__Miklós.xml')
-print(creations)
-file = "Benedek__Miklós_2016_010.txt"
-notxt_filename = file.split('.txt')[0]
-splitted_filename = notxt_filename.split('_')
-formatted_filename = splitted_filename[0] + '__' + splitted_filename[2]
+# file = input("Továbbdarabolni kívánt fájl: (fajlnev.txt)")
+# print(file)
+# file = "Abafáy-Deák__Csillag_2020_002.txt"
+# xml = input("XML fájl neve: (fajlnev.xml)")
+# name = get_formattedname('xmloutputs/Csobánka__Zsuzsa_Emese.xml')
+# print(name)
+# xml = "Abafáy-Deák__Csillag.xml"
+# print(xml)
+# creations = get_creationsbyfile(f'xmloutputs/{xml}')
+# print(creations)
+# notxt_filename = file.split('.txt')[0]
+# splitted_filename = notxt_filename.split('_')
+# formatted_filename = splitted_filename[0] + '__' + splitted_filename[2]
 
 
-def current_titles(creationsbyfile, f_filename):
+def current_titles(creationsbyfile, formattedfilename, notxtfilename):
     global currenttitle
     for key in creationsbyfile.keys():
         # formatted_key = key.split('tiszataj_')[1]
-        year = notxt_filename.split(f_filename)[1][1:]
+        print(notxtfilename)
+        print(formattedfilename)
+        year = notxtfilename.split(formattedfilename)[1][1:]
+        print(year)
         if year in key:
             currenttitle = creationsbyfile[key]
     return currenttitle
 
 
-def multiple_splitter(filename, creationsbyfile, f_filename):
+def multiple_splitter(filename, creationsbyfile, creator_name_f):
     global titles
-    current_title = current_titles(creationsbyfile, f_filename)
-    #for currentTitle in creationsbyfile.values():
+    notxt_filename = filename.split('.txt')[0]
+    # splitted_filename = notxt_filename.split('_')
+    # f_filename = splitted_filename[0] + '__' + splitted_filename[2]
+    current_title = current_titles(creationsbyfile, creator_name_f, notxt_filename)
+    # for currentTitle in creationsbyfile.values():
     if ";" in current_title:
         titles = current_title.split(';')
+        print(titles)
 
     for i in range(len(titles)):
         titles[i] = titles[i].strip()
         if '[' in titles[i]:
             splitted = titles[i].split('[')
-            titles[i] = splitted[0]
+            titles[i] = splitted[0].strip()
         if '(' in titles[i]:
             splitted = titles[i].split('(')
-            titles[i] = splitted[0]
+            titles[i] = splitted[0].strip()
         if ':' in titles[i]:
             titles[i] = titles[i].replace(':', '').strip()
         if '?' in titles[i]:
             titles[i] = titles[i].replace('?', '').strip()
+        titles[i] = titles[i].replace('  ', ' ')
 
+    print(titles)
     with open(f'txtoutputs/{filename}', encoding='windows-1250', errors='ignore') as in_file:
         filelines = in_file.readlines()
 
@@ -48,21 +64,21 @@ def multiple_splitter(filename, creationsbyfile, f_filename):
                 titles.remove(first_title)
 
                 if len(titles) == 0:
-                    with open(f'txtoutputs/{formatted_filename}_{first_title}.txt', 'w', encoding='windows-1250',
+                    with open(f'txtoutputs/{creator_name_f}_{first_title}.txt', 'w', encoding='windows-1250',
                               errors="ignore") as output:
                         output.writelines(filelines[i:])
+                        print("Kész!")
                     break
 
                 for j in range(i, len(filelines)):
                     isthereanother = any((k for k in titles if filelines[j].rstrip("\n").startswith(k)))
 
                     if isthereanother is True:
-                        with open(f'txtoutputs/{formatted_filename}_{first_title}.txt', 'w', encoding='windows-1250',
+                        with open(f'txtoutputs/{creator_name_f}_{first_title}.txt', 'w', encoding='windows-1250',
                                   errors="ignore") as output:
                             output.writelines(filelines[i:j])
 
                         i = j + 1
                         break
 
-
-multiple_splitter(file, creations, formatted_filename)
+# multiple_splitter(file, creations)
